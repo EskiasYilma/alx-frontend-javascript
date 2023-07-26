f984806c0a2f060f7ef6eb9949779674e47fbf89
exuszee <exuszee@gmail.com>
2023-07-26 17:39:43 +0300

    task 3 used all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,5 @@ export default async function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((values) => {
     const result = `${values[0].body} ${values[1].firstName} ${values[1].lastName}`;
     console.log(result);
-  }).catch(() => console.log('Signup system offline:'));
+  }).catch(() => console.log('Signup system offline'));
 }
