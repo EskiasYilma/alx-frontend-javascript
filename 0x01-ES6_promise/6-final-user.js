137bd96bb20e9122cebb0a1e6b785820375f4273
exuszee <exuszee@gmail.com>
2023-07-26 19:06:29 +0300

    task 6 fix using map nad allSettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,6 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => values).catch((error) => error);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: 'fulfilled',
+          value: result.value,
+        };
+      }
+      return {
+        status: 'rejected',
+        value: result.reason,
+      };
+    }));
 }
