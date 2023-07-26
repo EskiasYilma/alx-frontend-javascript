24b26ddf519e2a4681d8a6ded70a825778a35323
exuszee <exuszee@gmail.com>
2023-07-26 19:13:08 +0300

    task 6 fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,17 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: 'fulfilled',
-          value: result.value,
-        };
-      }
-      return {
-        status: 'rejected',
-        value: result.reason.message,
-      };
-    }));
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => values).catch((error) => error);
 }
