eabfe2dc59d6d1c1d9393122eaac867b3a6b7fa1
exuszee <exuszee@gmail.com>
2023-07-27 18:48:12 +0300

    5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  // Getters
+  get sqft() {
+    return this._sqft;
+  }
+}
