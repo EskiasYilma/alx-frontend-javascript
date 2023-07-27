adca5000611e60e2abe7776fa5a47cdad677dd7c
exuszee <exuszee@gmail.com>
2023-07-27 18:29:48 +0300

    3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,6 +1,37 @@
 export default class Currency {
-    constructor(code, name) {
-        this._code = code;
-        this._name = name;
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  // Setters
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  // Getters
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
     }
+  }
+
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  // Custom Method
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
