30c2bf20f61f42ee1dd033dbf0e2261500d4be80
exuszee <exuszee@gmail.com>
2023-08-01 14:33:42 +0300

    8-9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const grMap = new Map();
+  grMap.set('Apples', 10);
+  grMap.set('Tomatoes', 10);
+  grMap.set('Pasta', 1);
+  grMap.set('Rice', 1);
+  grMap.set('Banana', 5);
+
+  return grMap;
+}
