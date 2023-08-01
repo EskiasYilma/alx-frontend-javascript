7262197eab3e20ebb81204e838c235e63785fbdf
exuszee <exuszee@gmail.com>
2023-08-01 13:14:15 +0300

    fixes

--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,3 +1,7 @@
-import setFromArray from "./6-set.js";
+import getListStudents from "./0-get_list_students.js";
+import getStudentIdsSum from "./3-get_ids_sum.js";
 
-console.log(setFromArray([12, 32, 15, 78, 98, 15]));
+const students = getListStudents();
+const value = getStudentIdsSum(students);
+
+console.log(value);
